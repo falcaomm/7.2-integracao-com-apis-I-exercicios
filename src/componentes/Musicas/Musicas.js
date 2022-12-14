952a850d7350765e0d5d9b020acb209dcba0be37
falcaomm <manu.m.falcao@gmail.com>
2022-12-13 21:46:02 -0300

    ex fixacao

--- src/componentes/Musicas/Musicas.js
+++ src/componentes/Musicas/Musicas.js
@@ -1,28 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Botao, ContainerInputs, ContainerMusicas, InputMusica, Musica } from './styled'
+import axios from 'axios'
 
-const musicasLocal = [{
-    artist: "Artista 1",
-    id: "1",
-    name: "Musica1",
-    url: "http://spoti4.future4.com.br/1.mp3"
-},
-{
-    artist: "Artista 2",
-    id: "2",
-    name: "Musica2",
-    url: "http://spoti4.future4.com.br/2.mp3"
-},
-{
-    artist: "Artista 3",
-    id: "3",
-    name: "Musica3",
-    url: "http://spoti4.future4.com.br/3.mp3"
-}]
 
 export default function Musicas(props) {
-    const [musicas, setMusicas] = useState(musicasLocal)
+    const [musicas, setMusicas] = useState([])
+    const [artista, setArtista] = useState('')
+    const [nomeMusica, setNomeMusica] = useState('')
+    const [url, setUrl] = useState('')
 
+    const getPlaylistTracks = (id) => {
+
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
+
+        const config = {
+            headers: {
+                Authorization: "manuela-falcao-barbosa"
+            }
+        }
+
+        axios
+            .get(url, config)
+            .then((response) => {
+                setMusicas(response.data.result.tracks);
+                getPlaylistTracks(props.playlist.id)
+            })
+            .catch((error) => {console.log(error.response); })
+    }
+
+    const addTrackToPlaylist = (id) => {
+
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks`
+        const config = {
+            headers: {
+                Authorization: "manuela-falcao-barbosa"
+            }
+        }
+        const body = {
+            name: nomeMusica,
+            artist: artista,
+            url: url
+        }
+
+        axios
+            .post(url, body, config)
+            .then((response) => {
+                console.log('foi');
+            })
+            .catch((error) => { console.log(error.response) })
+        
+        setArtista('')
+        setNomeMusica('')
+        setUrl('')
+    }
+
+    const removeTrackFromPlaylist = (id, trackId) => {
+
+        const url = `https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/${id}/tracks/${trackId}`
+        const config = {
+            headers: {
+                Authorization: "manuela-falcao-barbosa"
+            }
+        }
+
+        axios
+            .delete(url, config)
+            .then((response) => {
+                console.log('foi');
+            })
+            .catch((error) => { console.log("n foi") })
+
+    }
+
+    useEffect(() => {
+        getPlaylistTracks(props.playlist.id)
+    }, [])
+
+    
     return (
         <ContainerMusicas>
             <h2>{props.playlist.name}</h2>
@@ -31,14 +85,27 @@ export default function Musicas(props) {
                     <Musica key={musica.id}>
                         <h3>{musica.name} - {musica.artist}</h3>
                         <audio src={musica.url} controls />
-                        <button>X</button>
+                        <button
+                            onClick={() => {
+                                removeTrackFromPlaylist(props.playlist.id, musica.id)
+                            }}
+                        >X</button>
                     </Musica>)
             })}
             <ContainerInputs>
-                <InputMusica placeholder="artista" />
-                <InputMusica placeholder="musica" />
-                <InputMusica placeholder="url" />
-                <Botao>Adicionar musica</Botao>
+                <InputMusica
+                    value={artista}
+                    onChange={(e) => setArtista(e.target.value)}
+                    placeholder="artista" />
+                <InputMusica
+                    value={nomeMusica}
+                    onChange={(e) => setNomeMusica(e.target.value)}
+                    placeholder="musica" />
+                <InputMusica
+                    value={url}
+                    onChange={(e) => setUrl(e.target.value)}
+                    placeholder="url" />
+                <Botao onClick={() => addTrackToPlaylist(props.playlist.id)}>Adicionar musica</Botao>
             </ContainerInputs>
         </ContainerMusicas>
     )
--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -1,31 +1,39 @@
-import React, {  useState } from "react";
+import React, { useEffect, useState } from "react";
 import Musicas from "../Musicas/Musicas";
+import axios from "axios";
 
-const playlistsLocal = [
-    {
-        id: 1,
-        name: "Playlist 1"
-    },
-    {
-        id: 2,
-        name: "Playlist 2"
-    },
-    {
-        id: 3,
-        name: "Playlist 3"
-    },
-    {
-        id: 4,
-        name: "Playlist 4"
-    },
-]
+
 function Playlists() {
-    const [playlists, setPlaylists] = useState(playlistsLocal)
+    const [playlists, setPlaylists] = useState([])
+
+    const getAllPlaylists = () => {
+        const url = "https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists"
+        const config = {
+            headers: {
+                Authorization: "manuela-falcao-barbosa"
+            }
+        }
+
+        axios.get(url, config)
+            .then((response) => {
+                setPlaylists(response.data.result.list)
+                console.log(response.data.result.list);; 
+            })
+            .catch((error) => {
+                alert(error.response);
+            })
+    }
+
+    useEffect(() => {
+        getAllPlaylists()
+    }, [])
   
     return (
         <div>
             {playlists.map((playlist) => {
-                return <Musicas key={playlist.id} playlist={playlist}/>
+                return<Musicas
+                    key={playlist.id}
+                    playlist={playlist} />
             })}
 
         </div>
